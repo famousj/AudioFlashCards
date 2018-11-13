8c67179f56e9caef6b27c3ee1b366cea5aeff741
justin.leblanc <justin.leblanc@asynchrony.com>
2018-11-13 11:31:20 -0600

    WIP - Working on fixing the layout to scale for height (not width)

--- AudioFlashCards/Screens/Card/CardView.swift
+++ AudioFlashCards/Screens/Card/CardView.swift
@@ -17,6 +17,9 @@ class CardView: UIView {
     let instructionsText_unanswered = "Just say the answer!"
     let instructionsText_answered = "Tap anywhere to continue"
     
+    let horizontalMargin: CGFloat = 32
+    
+    
     weak var delegate: CardViewDelegate?
     
     override init(frame: CGRect) {
@@ -70,62 +73,96 @@ class CardView: UIView {
 
 private extension CardView {
     func layout() {
-        let horizontalMargin: CGFloat = 32
-
+        
         let verticalMargin: CGFloat = 60
-        let numberMargin: CGFloat = horizontalMargin * 2
         
         var constraints: [NSLayoutConstraint] = []
         
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
         addGestureRecognizer(tapRecognizer)
         
-        backgroundColor = .white
-
-        setupLabel(num1Label, font: Fonts.numberFont)
-        num1Label.textAlignment = .right
-        addSubview(num1Label)
-        constraints.append(contentsOf: createHorizontalAnchorConstraints(num1Label, constant: numberMargin))
-        constraints.append(num1Label.topAnchor.constraint(equalTo: topAnchor, constant: verticalMargin))
-
-        setupLabel(num2Label, font: Fonts.numberFont)
-        addSubview(num2Label)
-        constraints.append(createRightAnchorConstraint(num2Label, superview: self, constant: numberMargin))
-        constraints.append(constrainTopToBottom(bottomView: num2Label, topView: num1Label))
-
-        setupLabel(operationLabel, font: Fonts.numberFont)
-        addSubview(operationLabel)
-        constraints.append(operationLabel.rightAnchor.constraint(equalTo: num2Label.leftAnchor, constant: -horizontalMargin))
-        constraints.append(operationLabel.bottomAnchor.constraint(equalTo:num2Label.bottomAnchor))
-
-        barView.backgroundColor = .black
-        barView.isHidden = true
-        addSubview(barView)
-        barView.translatesAutoresizingMaskIntoConstraints = false
-        constraints.append(barView.heightAnchor.constraint(equalToConstant: 15))
-        constraints.append(contentsOf: createHorizontalAnchorConstraints(barView, constant: horizontalMargin))
-        constraints.append(constrainTopToBottom(bottomView: barView, topView: num2Label))
-    
-        setupLabel(answerLabel, font: Fonts.numberFont)
-        answerLabel.textAlignment = .right
-        addSubview(answerLabel)
-        constraints.append(contentsOf: createHorizontalAnchorConstraints(answerLabel, constant: numberMargin))
-        constraints.append(constrainTopToBottom(bottomView: answerLabel, topView: barView))
+        backgroundColor = .blue
+        
+        let problemContainerView = problemView
+        addSubview(problemContainerView)
+        problemContainerView.translatesAutoresizingMaskIntoConstraints = false
+        constraints.append(problemContainerView.topAnchor.constraint(equalTo: topAnchor, constant: verticalMargin))
+        constraints += createHorizontalAnchorConstraints(problemContainerView, constant: horizontalMargin)
+        
+        
+        
         
         setupLabel(instructionsLabel, font: Fonts.instructionFont)
         instructionsLabel.numberOfLines = 0
         instructionsLabel.textAlignment = .center
         addSubview(instructionsLabel)
         constraints.append(contentsOf: createHorizontalAnchorConstraints(instructionsLabel, constant: horizontalMargin))
-        constraints.append(constrainTopToBottom(bottomView: instructionsLabel, topView: answerLabel))
         
-        setupLabel(speechRecognitionLabel, font: Fonts.speechRecognitionFont)
-        speechRecognitionLabel.backgroundColor = .yellow
-        speechRecognitionLabel.numberOfLines = 0
-        addSubview(speechRecognitionLabel)
-        constraints.append(contentsOf: createHorizontalAnchorConstraints(speechRecognitionLabel, constant: horizontalMargin))
-        constraints.append(speechRecognitionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -verticalMargin))
+        let verticalSpacing: CGFloat = 32
+        constraints.append(instructionsLabel.topAnchor.constraint(equalTo: problemContainerView.bottomAnchor, constant: verticalSpacing))
+        constraints.append(constrainTopToBottom(bottomView: instructionsLabel, topView: problemContainerView))
+        
+        
+        constraints.append(instructionsLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -verticalMargin))
+        
+        //
+        //        setupLabel(speechRecognitionLabel, font: Fonts.speechRecognitionFont)
+        //        speechRecognitionLabel.backgroundColor = .yellow
+        //        speechRecognitionLabel.numberOfLines = 0
+        //        addSubview(speechRecognitionLabel)
+        //        constraints.append(contentsOf: createHorizontalAnchorConstraints(speechRecognitionLabel, constant: horizontalMargin))
+        //        constraints.append(speechRecognitionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -verticalMargin))
+        
         constraints.forEach{ $0.isActive = true }
+    }
+    
+    var problemView: UIView {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        var constraints: [NSLayoutConstraint] = []
+        
+        let multiplier: CGFloat = 1/5
+        
+        setupLabel(num1Label, font: Fonts.numberFont)
+        num1Label.backgroundColor = .orange
+        num1Label.textAlignment = .center
+        containerView.addSubview(num1Label)
+        constraints += [num1Label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+                        bottomConstraint(item: num1Label, toItem: containerView, multiplier: multiplier*1)]
+        
+        setupLabel(num2Label, font: Fonts.numberFont)
+        num2Label.backgroundColor = .brown
+        containerView.addSubview(num2Label)
+        constraints += [num2Label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+                        bottomConstraint(item: num2Label, toItem: containerView, multiplier: multiplier*2)]
+        
+        setupLabel(operationLabel, font: Fonts.numberFont)
+        containerView.addSubview(operationLabel)
+        constraints += [operationLabel.centerYAnchor.constraint(equalTo: num2Label.centerYAnchor),
+                        operationLabel.rightAnchor.constraint(equalTo: num2Label.leftAnchor, constant: -horizontalMargin)]
+        
+        barView.backgroundColor = .black
+        barView.isHidden = true
+        containerView.addSubview(barView)
+        barView.translatesAutoresizingMaskIntoConstraints = false
+        constraints += createHorizontalAnchorConstraints(barView, constant: 0)
+        constraints += [barView.heightAnchor.constraint(equalToConstant: 15),
+                        bottomConstraint(item: barView, toItem: containerView, multiplier: multiplier*3)]
+
+        setupLabel(answerLabel, font: Fonts.numberFont)
+        answerLabel.backgroundColor = .brown
+        containerView.addSubview(answerLabel)
+        constraints += [answerLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+                        bottomConstraint(item: answerLabel, toItem: containerView, multiplier: multiplier*4)]
+        
+        constraints.forEach{ $0.isActive = true }
+        
+        return containerView
+    }
+    
+    func bottomConstraint(item: UIView, toItem: UIView, multiplier: CGFloat) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: item, attribute: .centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: toItem, attribute: .bottom, multiplier: multiplier, constant: 0)
     }
     
     func setupLabel(_ label: UILabel, font: UIFont) {
@@ -145,7 +182,7 @@ private extension CardView {
     func createLeftAnchorConstraint(_ view: UIView, superview: UIView, constant: CGFloat) -> NSLayoutConstraint {
         return view.leftAnchor.constraint(equalTo: superview.leftAnchor, constant: constant)
     }
-
+    
     func createRightAnchorConstraint(_ view: UIView, superview: UIView, constant: CGFloat) -> NSLayoutConstraint {
         return view.rightAnchor.constraint(equalTo: superview.rightAnchor, constant: -constant)
     }
